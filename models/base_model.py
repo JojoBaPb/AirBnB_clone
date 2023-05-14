55bdb61d2eb6977469965240a1c5afb869f3627d
JojoBaPb <sipo.jmudenda@gmail.com>
2023-05-14 16:52:16 +0200

    3

---
+++ models/base_model.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Module for BaseModel class
+"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Class for all common attributes for other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize BaseModel class
+        """
+
+    from models import storage
+    if not kwargs:
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        storage.new(self)
+    else:
+        for key, value in kwargs.items():
+            if key != '__class__':
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """
+        Prints string rep of BaseModel object.
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                self.__dict__)
+
+    def save(self):
+    """
+    Updates 'updated_at' with the current datetime
+    """
+    from models import storage
+    self.updated_at = datetime.now()
+    storage.save()
+
+    def to_dict(self):
+    """
+    Returns dictionary containing all keys/values of __dict__
+    of the instance:
+
+    * ONLY instance attributes set will be returned
+    * a key __class__ added with class name of object
+    * 'created_at' and 'updated_at' to be converted to string object in ISO
+    format
+    """
+    dict1 = self.__dict__.copy()
+    dict1["__class__"] = self.__class__.__name__
+    for x, y in self.__dict__.items():
+        if x in ("created_at", "updated_at"):
+            y = self.__dict__[x].isoformat()
+            dict1[x] = y
+    return dict1
